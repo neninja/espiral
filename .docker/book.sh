667a21c50f9a1ccf30690d3ae08009422f866bf5
Felipe Silva <felipe.dacs@gmail.com>
2022-04-28 01:07:45 -0300

    :zzz: simplifica build pandoc

--- .docker/book.sh
+++ .docker/book.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-pandoc -s -N --variable "geometry=margin=1in" \
-    --metadata-file=content/meta.yml \
-    content/1_introducao/README.md \
-    content/2_algoritmos/README.md \
-    content/3_git/README.md \
-    content/4_react/README.md \
-    content/5_lumen/README.md \
-    --toc -o intro-dev-web.pdf
+pandoc -s -N --metadata-file=content/meta.yml --toc \
+    --variable "geometry=margin=1in" \
+    content/*/README.md \
+    -o intro-dev-web.pdf
 
-pandoc -s -N --variable "geometry=margin=1in" \
-    --metadata-file=content/meta.yml \
-    content/1_introducao/README.md \
-    content/2_algoritmos/README.md \
-    content/3_git/README.md \
-    content/4_react/README.md \
-    content/5_lumen/README.md \
-    --toc -o intro-dev-web.epub
+pandoc -s -N --metadata-file=content/meta.yml --toc \
+    --variable "geometry=margin=1in" \
+    content/*/README.md \
+    -o intro-dev-web.epub
--- .docker/html.sh
+++ .docker/html.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-pandoc -s -N --variable "geometry=margin=1in" \
-    --metadata-file=content/meta.yml \
-    content/1_introducao/README.md \
-    content/2_algoritmos/README.md \
-    content/3_git/README.md \
-    content/4_react/README.md \
-    content/5_lumen/README.md \
-    --toc -o intro-dev-web.html
+pandoc -s -N --metadata-file=content/meta.yml --toc \
+    --variable "geometry=margin=1in" \
+    content/*/README.md \
+    -o intro-dev-web.html
